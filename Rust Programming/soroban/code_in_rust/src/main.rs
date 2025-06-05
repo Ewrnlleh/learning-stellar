0df9ec2eb39efa79ab7e4e7036529296074c99ef
ewrnlleh <ewrnlleh@gmail.com>
2025-06-05 12:46:51 +0300

    learned struct and enum

--- Rust Programming/soroban/code_in_rust/src/main.rs
+++ Rust Programming/soroban/code_in_rust/src/main.rs
@@ -18,5 +18,47 @@ fn main() {
 
     // to print the output out, we need to write a macro => println! macro
 
-    println!("{}", x)
+    println!("{}", x); // -> this is the first version
+
+    let z: String = String::from("Hello, Soroban!"); //mutable string
+    let y: &str = "Hello, Stellar!"; //immutable string
+
+    println!("{y}");
+    println!("{z}"); // tihs is the second version
+
+    // functions -> fn - > pub fn - > public fn - > private
+
+    // pub fn event (name: String) {
+        // let name: String = String::from ("James");
+        // println!("{}", name);
+    //}
+
+    let e: EventForKids = EventForKids {
+        name: String = String::from("KidsCo"),
+        date: String = String::from("04.03.2024"),
+        number_of_participants: u32 = 1000,
+        place: String = String::from("NY, USA")
+    };
+
+    // add your enum in here...
+
+
+
+}
+
+
+// structs -> compiling many items in one class
+
+struct EventForKids {
+    name: String,
+    date: String,
+    number_of_participants: u32,
+    place: String
+}
+
+//enums -> compiling errors in once class
+enum ErrorsForEvent{
+    NoEvent,
+    CancelledEvent,
+    EventType
 }
