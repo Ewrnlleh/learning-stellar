286857ec9a061dd63a36740892391db4d004cc84
ewrnlleh <ewrnlleh@gmail.com>
2025-06-05 11:52:37 +0300

    new code in rust with cargo

---
+++ Rust Programming/soroban/code_in_rust/src/main.rs
@@ -0,0 +1,22 @@
+// mkdir soroban
+// cd soroban
+// cargo new code_in_rust
+
+
+// go to terminal -> cargo build -> it will add target, cargo.lock, and .gitingnore
+
+
+
+fn main() {
+    // variables
+    // let -> immutable
+    // let mut -> mutable
+
+    // basic data types -> Rust is a type language
+
+    let x: i32 = 16;
+
+    // to print the output out, we need to write a macro => println! macro
+
+    println!("{}", x)
+}
---
+++ Rust Programming/soroban/code_in_rust/target/debug/deps/code_in_rust-193beb3d46430816.d
@@ -0,0 +1,5 @@
+/home/nocrest/Learning-Stellar/Rust Programming/soroban/code_in_rust/target/debug/deps/code_in_rust-193beb3d46430816: src/main.rs
+
+/home/nocrest/Learning-Stellar/Rust Programming/soroban/code_in_rust/target/debug/deps/code_in_rust-193beb3d46430816.d: src/main.rs
+
+src/main.rs:
