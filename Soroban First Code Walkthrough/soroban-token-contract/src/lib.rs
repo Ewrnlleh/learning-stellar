a35e8a36327546bb8036ae955311c1c23c15c35b
ewrnlleh <ewrnlleh@gmail.com>
2025-06-05 13:17:56 +0300

    created soroban-token-contract project

---
+++ Soroban First Code Walkthrough/soroban-token-contract/src/lib.rs
@@ -0,0 +1,29 @@
+# ! [no_std]
+
+mod admin;
+mod allowance;
+mod balan+;
+mod contract;
+mod metadata;
+mod storage_types;
+
+pubuse crate::contract::TokenCIient;
+
+
+
+/* 
+pub fn add(left: u64, right: u64) -> u64 {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
+ */
